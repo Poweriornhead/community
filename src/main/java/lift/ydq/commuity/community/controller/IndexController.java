4b49d093d6764358944e98fd943bf76136d51b55
unknown <2531382726@qq.com>
2022-03-31 21:05:15 +0800

    adddatabase

--- src/main/java/lift/ydq/commuity/community/controller/IndexController.java
+++ src/main/java/lift/ydq/commuity/community/controller/IndexController.java
@@ -1,8 +1,15 @@
 package lift.ydq.commuity.community.controller;
 
+import lift.ydq.commuity.community.mapper.UserMapper;
+import lift.ydq.commuity.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author YDQ
@@ -10,8 +17,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+
+        }
+
+
         return "index";
     }
 }
---
+++ src/main/java/lift/ydq/commuity/community/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package lift.ydq.commuity.community.mapper;
+
+import lift.ydq.commuity.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author YDQ
+ * @create 2022-03-31 19:15
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
